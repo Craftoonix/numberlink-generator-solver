6241fe733016d9d2390b2ad50dfec6f179567e6f
Craftoonix <j.j.lelipaly@gmail.com>
2025-10-10 21:31:52 +0200

    Added a constants file

---
+++ include/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+static const short MAX_CELLS = 20;    // maximum height & width of the puzzle
+static const short ARGUMENTS = 2;    // number of command line arguments
+
+#endif // CONSTANTS_H
